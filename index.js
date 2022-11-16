445815653f371eade97e7babcdbeddc070fb1363
unknown <ekaramulhasan063@gmail.com>
2022-11-16 06:28:38 +0600

    add server collection

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rlfbbtk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        const serverCollection = client.db('travel-care').collection('services');
+
+       
+    }
+    finally{}
+}
+run().catch(console.dir);
+
 
 
 
